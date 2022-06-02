675ca40c0fd14c13b048d26f08b27a48dffd09fb
virender.jangra28 <virender.jangra28@gmail.com>
2022-06-02 23:48:13 +0530

    add validation on reg and login page

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConstantService } from 'src/app/services/constant.service';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +12,18 @@ export class LoginComponent implements OnInit {
   isLogin : boolean = true;
   loginEmail = '';
   loginPassword = '';
+  regMobile = '';
+  regEmail = '';
+  regPassword = '';
+  regConfirmPassword = '';
   showPwd : boolean = false;
+  isError : boolean = false;
+  errorText = '';
+  isValidForLogin : boolean = false;
+  isValidForReg : boolean = false;
   constructor(
-    private _router : Router
+    private _router : Router,
+    private _constantService : ConstantService
   ) { }
 
   ngOnInit(): void {
@@ -21,16 +31,153 @@ export class LoginComponent implements OnInit {
     
   }
   changeMode(){
+    this.isError = false;
+    this.errorText = '';
     this.isLogin = !this.isLogin
   }
   loginSubmit(){
+    this.logiVaidation();
+    if(!this.isValidForLogin){
+      return;
+    }
     if(this.loginEmail == 'admin' && this.loginPassword == '123') {
       alert('Login Success');
       this._router.navigate(['/approvals']);
 
     } else {
-      alert('Wrong cred !');
+      var emailLogin = document.getElementsByClassName('inputEmailField')[0];
+      var passwordLogin = document.getElementsByClassName('inputPassField')[0];
+      this.isError = true;
+      this.errorText = 'Wrong Credentials !';
+      emailLogin.classList.add('error');
+      passwordLogin.classList.add('error')
+      setTimeout(() => {
+        emailLogin.classList.remove('error');
+        passwordLogin.classList.remove('error')
+      }, 500);
+      this.autoHideError();
+      return;
     }
 
   }
+
+  // ============== Registration Process Start ==========================
+
+  registration() {
+    this.validate();
+    if(!this.isValidForReg) {
+        return;
+    }
+    alert('hi')
+    var register : any = {}
+    register['email'] = this.regEmail;
+    register['mobileNumber'] = this.regMobile;
+    register['password'] = this.regPassword;
+    this._constantService.fetchDataApi(this._constantService.getNewAdminRegistrationServiceUrl(), register).subscribe(data => {
+      var response = data;
+    })
+    
+  }
+
+  //  ===================== Validation for Registration ======================
+
+  validate() {
+        if(this.regMobile == ''){
+          this.isValidForReg = false;
+            var mobRegister = document.getElementsByClassName('inputFieldForMobileReg')[0];
+            this.isError = true;
+            this.errorText = 'Please enter valid Mobile Number!';
+            mobRegister.classList.add('error');
+            setTimeout(() => {
+              mobRegister.classList.remove('error');
+            }, 500);
+            this.autoHideError();
+            return;
+        } else if(this.regEmail == ''){
+          this.isValidForReg = false;
+          var mailRegister = document.getElementsByClassName('inputFieldForEmailReg')[0];
+          this.isError = true;
+          this.errorText = 'Please enter valid Email Address!';
+          mailRegister.classList.add('error');
+          setTimeout(() => {
+            mailRegister.classList.remove('error');
+          }, 500);
+          this.autoHideError();
+          return;
+        } else if(this.regPassword == ''){
+          this.isValidForReg = false;
+        var passRegister = document.getElementsByClassName('inputFieldForPassReg')[0];
+        this.isError = true;
+        this.errorText = 'Please enter Password!';
+        passRegister.classList.add('error');
+        setTimeout(() => {
+          passRegister.classList.remove('error');
+        }, 500);
+        this.autoHideError();
+        return;
+        } else if(this.regConfirmPassword == ''){
+          this.isValidForReg = false;
+        var confirmPassRegister = document.getElementsByClassName('inputFieldForRepeatPassReg')[0];
+        this.isError = true;
+        this.errorText = 'Please re-type Password!';
+        confirmPassRegister.classList.add('error');
+        setTimeout(() => {
+          confirmPassRegister.classList.remove('error');
+        }, 500);
+        this.autoHideError();
+        return;
+        } else if (this.regPassword != this.regConfirmPassword) {
+          this.isValidForReg = false;
+        var confirmPassRegister = document.getElementsByClassName('inputFieldForRepeatPassReg')[0];
+        this.isError = true;
+        this.errorText = 'Please type same Password!';
+        confirmPassRegister.classList.add('error');
+        setTimeout(() => {
+          confirmPassRegister.classList.remove('error');
+        }, 500);
+        this.autoHideError();
+        return;
+        } else {
+          this.isValidForReg = true;
+        }
+  }
+
+  // ======================== Auto Hide Error Text ===========================
+  autoHideError() {
+    setTimeout(() => {
+      this.isError = false;
+      this.errorText = '';
+    }, 5000);
+  }
+
+  //  ===================== Validation for Login ======================
+
+  logiVaidation() {
+    if(this.loginEmail == ''){
+      this.isValidForLogin = false;
+        var emailLogin = document.getElementsByClassName('inputEmailField')[0];
+        this.isError = true;
+        this.errorText = 'Please enter valid Email Address!';
+        emailLogin.classList.add('error');
+        setTimeout(() => {
+          emailLogin.classList.remove('error');
+        }, 500);
+        this.autoHideError();
+        return;
+    } else if(this.loginPassword == ''){
+      this.isValidForLogin = false;
+        var passwordLogin = document.getElementsByClassName('inputPassField')[0];
+        this.isError = true;
+        this.errorText = 'Please enter Password!';
+        passwordLogin.classList.add('error');
+        setTimeout(() => {
+          passwordLogin.classList.remove('error');
+        }, 500);
+        this.autoHideError();
+        return;
+    }else {
+      this.isValidForLogin = true;
+    }
+  }
+
 }
--- src/app/services/constant.service.ts
+++ src/app/services/constant.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,32 @@ import { Injectable } from '@angular/core';
 })
 export class ConstantService {
 
-  constructor() { }
+  master_service_url : any;
+  
+  constructor(
+    private http : HttpClient,
+  ) { 
+    this.master_service_url = "https://skinfotechies.in/demo/voip/api";
+  }
+
+
+//============================== Common fetchData Api ===============================
+  fetchDataApi(url : string, params : any, headers = new Headers({ 'Content-Type' : 'application/json'}) ){
+          var options = {
+          headers : new HttpHeaders({ 'Content-Type' : 'application/json'}),
+        };
+        if(params == ''){
+          params = '';
+        }
+
+        return this.http.post(url , params, options);
+}
+
+// ====================== API Section Start ======================================
+
+getNewAdminRegistrationServiceUrl(){
+  return this.master_service_url + "/admin-registration.php"
+}
+
+
 }
